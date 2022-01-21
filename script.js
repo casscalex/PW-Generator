600ae5abc1d4bdfde883e38c4c3f06915b6bbd0d
casscalex <casscalex@outlook.com>
2022-01-20 22:35:46 -0600

    went in a different direction and mixed everything up

--- script.js
+++ script.js
@@ -1,56 +1,70 @@
-// Assignment Code
+// Assignment code here
 
-//Arrays
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-var characters = ['!','@','#','$','%','^','&','*']
+// randomize characters
+function randomLower() {
+    const lowers = 'abcdefghijklmnopqrstuvwxyz'
+    return lowers[Math.floor(Math.random() * lowers.length)];
+}
+
+function randomUpper() {
+    const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return uppers[Math.floor(Math.random() * uppers.length)];
+}
+
+function randomNumber() {
+    const numbers = '0123456789'
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function randomSymbol() {
+    const symbols = '!@#$%^&*'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 
+// Generate Password Function
+
+
+function generatePassword() {
+
+    //password length
+    let passLength = prompt("How long would you like your Password to be? (Pick a number between 8 and 128)");
+    if (passLength < 8 || passLength > 128) {
+     alert("Your password must be beween 8 and 128 characters, try again!")
+    }
+
+    //password characters 
+    let confirmLower = confirm("Click OK if you want to include lowercase letters in your password")
+    let confirmUpper = confirm("Click OK if you want to include UPPERCASE letters in your password")
+    let confirmNumber = confirm("Click OK if you want to include Numbers in your password")
+    let confirmSymbol = confirm("Click OK if you want to include Symbols in your password")
+
+    if (confirmLower) {
+        
+    }
+    if (confirmUpper) {
+
+    }
+    if (confirmNumber) {
+
+    }
+    if (confirmSymbol) {
+
+    }
+
+    // for loop 
+    const randomType = randomLower + randomUpper + randomNumber + randomSymbol;
+    for (let i = 0; i < passLength; i += randomType) {
+
+    }
+}
+
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
-    // password length loop
-    var passLength = promt('How long would you like your password to be? Select a number between 8 and 128');
-
-    while (passLength <= 7 || passLength >= 129) {
-        alert('Password length must be between 8-128 characters, try again!');
-        var passLength = promt('How long would you like your password to be? Select a number between 8 and 128');
-
-    alert('Your password will be ${passLength} long')
-    }
-
-    // Determine critera for Password
-    var confirmUpper = confirm('Click OK if you want to use UPERCASE letters');
-    var confrimLower = confirm('Click OK if you want to use lowercase letters');
-    var confirmNumbers = confirm('Click OK if you want to use NUMBERS')
-    var confrimCharacters = confirm('Click OK if you want to us CHARACTERS')
-
-    while (confirmUpper === flase && confrimLower === flase && confirmNumbers === flase && confrimCharacters === flase) {
-        alert("You must choose at least one critera, try again!");
-        var confirmUpper = confirm('Click OK if you want to use UPERCASE letters');
-        var confrimLower = confirm('Click OK if you want to use lowercase letters');
-        var confirmNumbers = confirm('Click OK if you want to use NUMBERS')
-        var confrimCharacters = confirm('Click OK if you want to us CHARACTERS')
-    }
-
-    var passCritera = []
-    if (confirmUpper) {
-        passCritera = passCritera.concat(upperCase)
-    }
-    if (confrimLower) {
-        passCritera = passCritera.concat(lowerCase)
-    }
-    if (confirmNumbers) {
-        passCritera = passCritera.concat(numbers)
-    }
-    if (confrimCharacters) {
-        passCritera = passCritera.concat(characters)
-    }
-
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
